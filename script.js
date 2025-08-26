22a217868d7c38b2fd5f04a3795c4e232ae01773
jamato01 <jarrett.m.torres@gmail.com>
2025-08-26 13:44:31 -0400

    Initialize event listener for buttons

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+// DOM Elements
+const container = document.querySelector('.container');
+const displayScreen = document.querySelector('.display');
+
+container.addEventListener('click', (event) => {
+    let target = event.target;
+
+    switch (target.id) {
+        case 'one':
+            break;
+        case 'two':
+            break;
+        case 'three':
+            break;
+        case 'four':
+            break;
+        case 'five':
+            break;
+        case 'six':
+            break;
+        case 'seven':
+            break;
+        case 'eight':
+            break;
+        case 'nine':
+            break;
+        case 'zero':
+            break;
+        case 'decimal':
+            break;
+        case 'plus':
+            break;
+        case 'minus':
+            break;
+        case 'multiply':
+            break;
+        case 'divide':
+            break;
+        case 'equals':
+            break;
+        case 'clear':
+            break;
+        case 'backspace':
+            break;
+    }
+});
+
 // Helper Functions
 
 function add(a, b) {
