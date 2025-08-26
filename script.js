4adfd01b6059e16db41d66118803a83e6cd58675
jamato01 <jarrett.m.torres@gmail.com>
2025-08-26 13:32:13 -0400

    Create add, subtract, multiply, divide functions

---
+++ script.js
@@ -0,0 +1,22 @@
+// Helper Functions
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(6,4));
+console.log(subtract(6,4));
+console.log(multiply(6,4));
+console.log(divide(6,4));
